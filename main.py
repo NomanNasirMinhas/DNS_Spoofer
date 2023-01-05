5a214a2f0fd101c1585ac598a1ad8262702e2ce3
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2023-01-05 22:28:07 +0500

    Self Signed Cetificate creation

--- main.py
+++ main.py
@@ -1,7 +1,69 @@
+import datetime
 import netfilterqueue
 import subprocess
 import argparse
 import scapy.all as scapy
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+def generate_certificate():
+    # Generate a new private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    # Serialize the private key to PEM format
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    # Extract the public key from the private key
+    public_key = private_key.public_key()
+
+    # Serialize the public key to PEM format
+    public_key_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    # Create a self-signed certificate using the private key and public key
+    certificate = x509.CertificateBuilder().subject_name(
+        x509.Name([
+            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, u"example.com"),
+        ])
+    ).issuer_name(
+        x509.Name([
+            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, u"example.com"),
+        ])
+    ).public_key(
+        public_key
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName(u"example.com")]),
+        critical=False
+    ).sign(private_key, hashes.SHA256(), default_backend())
+
+    # Serialize the certificate to PEM format
+    certificate_pem = certificate.public_bytes(serialization.Encoding.PEM)
+
+    return certificate_pem, private_key_pem, public_key_pem
+    # Replace the original certificate fields in the DNS response with the new certificate
+    # (assuming the DNS response is stored in a variable called "response")
+    # response.certificate = certificate_pem
+    # response.private_key = private_key_pem
+    # response.public_key = public_key_pem
+
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -14,6 +76,7 @@ def parse_arguments():
     mode.add_argument('-f', '--forward', action='store', help='Forward Packets')
 
     parser.add_argument('-s', '--server', type=str, required=True, help='Spoofed Server IP Address')
+    parser.add_argument('-c', '--cert', action='store', help='Generate a self-signed certificate')
     return parser.parse_args()
 
 def process_packet(packet):
